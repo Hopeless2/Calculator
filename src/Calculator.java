1f7d5277023efa3ada7bba70aef395cb81bd61dc
Hopeless2 <89778966354@mail.ru>
2021-07-28 23:28:37 +0300

    Commit init

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+import java.util.function.*;
+
+public class Calculator {
+    static protected Supplier<Calculator> instance = Calculator::new ;
+    static protected BinaryOperator<Integer> plus = (x, y) -> x + y;
+    static protected BinaryOperator<Integer> minus = (x, y) -> x - y;
+    static protected BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    // делить на ноль нельзя, отсюда  java.lang.ArithmeticException
+    static protected BinaryOperator<Integer> devide = (x, y) -> y == 0 ? y : x / y ;
+    static protected UnaryOperator<Integer> pow = x -> x * x;
+    static protected UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    static protected Predicate<Integer> isPositive = x -> x > 0;
+    static protected Consumer<Integer> println = System.out::println;
+}
